93c1b939b293bc668a59e1fbe5bb474c4c6776d3
Will Worth <will.worth@gmail.com>
2019-08-12 09:05:24 +0200

    full crud working

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -9,7 +9,42 @@ import Grid from "@material-ui/core/Grid";
 import useTodoState from "./hooks/useTodoState";
 
 function TodoApp() {
-  return <h1>TODO</h1>;
-}
+  const initialTodos = JSON.parse(window.localStorage.getItem("todos") || "[]");
+  const { todos, addTodo, removeTodo, toggleTodo, editTodo } = useTodoState(
+    initialTodos
+  );
 
+  useEffect(() => {
+    window.localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  return (
+    <Paper
+      style={{
+        padding: 0,
+        margin: 0,
+        height: "100vh",
+        backgroundColor: "#fafafa"
+      }}
+      elevation={0}
+    >
+      <AppBar color='primary' position='static' style={{ height: "64px" }}>
+        <Toolbar>
+          <Typography color='inherit'>TODOS WITH HOOKS</Typography>
+        </Toolbar>
+      </AppBar>
+      <Grid container justify='center' style={{ marginTop: "1rem" }}>
+        <Grid item xs={11} md={8} lg={4}>
+          <TodoForm addTodo={addTodo} />
+          <TodoList
+            todos={todos}
+            removeTodo={removeTodo}
+            toggleTodo={toggleTodo}
+            editTodo={editTodo}
+          />
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+}
 export default TodoApp;
